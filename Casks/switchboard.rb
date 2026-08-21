73303da9146ae1ec047631ce08820b6bacb1ab33
Josh Cole <saikojosh@gmail.com>
2026-08-21 15:21:00 +0100

    switchboard 0.8.1

--- Casks/switchboard.rb
+++ Casks/switchboard.rb
@@ -1,6 +1,6 @@
 cask "switchboard" do
-  version "0.8.0"
-  sha256 "d48b267e29c562489839e4b104f8b1d6662aad21fea5b9f4bb1370aa7caa53ec"
+  version "0.8.1"
+  sha256 "f87372b3fa5f7e635694db0c7971e58479e3bed0bcfbd52a661345dddcaaee23"
 
   url "https://github.com/Recombix/switchboard-releases/releases/download/v#{version}/Switchboard-#{version}-arm64.dmg"
   name "Switchboard"
