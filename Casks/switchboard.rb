aba9c1971bed6c53a0c613893b739b53fc48d8ce
Josh Cole <saikojosh@gmail.com>
2026-09-03 22:57:18 +0100

    switchboard 0.9.5

--- Casks/switchboard.rb
+++ Casks/switchboard.rb
@@ -1,6 +1,6 @@
 cask "switchboard" do
-  version "0.9.4"
-  sha256 "03d6685e8d279bf1c2b0ffa4782fc6579d2b7fdb7f975177cb382585062df435"
+  version "0.9.5"
+  sha256 "70bc963c55b9f371626eef1de78b6fd8867025b9d8f5793f09ff5d1fa426dec4"
 
   url "https://github.com/Recombix/switchboard-releases/releases/download/v#{version}/Switchboard-#{version}-arm64.dmg"
   name "Switchboard"
